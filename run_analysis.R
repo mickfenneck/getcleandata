8e54b24605e1f4342fb31d0f3dea3a40fc7b6aba
Michele Sordo <michele.sordo@gmail.com>
2014-04-19 18:34:45 +0200

    right header

--- run_analysis.R
+++ run_analysis.R
@@ -6,11 +6,13 @@ run_analysis <- function(path) {
     titles <- read.table(paste(path,"/features.txt",sep=""))
     
     trainx <- read.table(paste(path,"train/X_train.txt",sep=""))
+    names(trainx) <- titles$V2
     trainy <- read.table(paste(path,"train/y_train.txt",sep=""))
     trainy <-  merge(x=trainy,y=activity_labels,by.x="V1",by.y="V1")
     train <- cbind(trainy[2],trainx)
     
     testx <- read.table(paste(path,"test/X_test.txt",sep=""))
+    names(testx) <- titles$V2
     testy <- read.table(paste(path,"test/y_test.txt",sep=""))
     testy <- join <- merge(x=testy,y=activity_labels,by.x="V1",by.y="V1")
     test <- cbind(testy[2],testx)
