bacbf99f9b112422b8f2664fb3cea915573c98ff
Michele Sordo <michele.sordo@gmail.com>
2014-04-19 20:05:10 +0200

    partially right dataset... wrong merge :(

--- run_analysis.R
+++ run_analysis.R
@@ -2,24 +2,27 @@
 # data <- run_analysis(paste(getwd(),"/UCI HAR Dataset/",sep=""))
 
 run_analysis <- function(path) {
+    #leggo label e titoli
     activity_labels <-read.table(paste(path,"/activity_labels.txt",sep=""))
     titles <- read.table(paste(path,"/features.txt",sep=""))
     
+    #leggo le x
     trainx <- read.table(paste(path,"train/X_train.txt",sep=""))
-    names(trainx) <- titles[2]
-    trainy <- read.table(paste(path,"train/y_train.txt",sep=""))
-    trainy <-  merge(x=trainy,y=activity_labels,by.x="V1",by.y="V1")
-    train <- cbind(trainy[2],trainx)
-    
     testx <- read.table(paste(path,"test/X_test.txt",sep=""))
-    names(testx) <- titles[2]
-    testy <- read.table(paste(path,"test/y_test.txt",sep=""))
-    testy <- join <- merge(x=testy,y=activity_labels,by.x="V1",by.y="V1")
-    test <- cbind(testy[2],testx)
+    x <- rbind(trainx,testx)
+    names(x) <- titles$V2
+    eraseCol <- grep("mean()|std()",names(x),invert=TRUE)
+    x[eraseCol] <- list(NULL)
     
-    #append the 2ns table to the 1st 
-    data <- rbind(test,train)
-    #change name of the first column to labels
-    names(data)[1]<-"labels"
+    #leggo le y e sostituisco con labels labels
+    trainy <- read.table(paste(path,"train/y_train.txt",sep=""))
+    testy <- read.table(paste(path,"test/y_test.txt",sep=""))
+    y <- rbind(trainy,testy)
+    y <- merge(x=y,y=activity_labels,by.x="V1",by.y="V1") #non fa bene il merge <- porcoo#######
+    names(y)[2] <- "Labels"
+    
+    #unisco y a x
+    data <- cbind(y[2],x)
+
     data
 }
