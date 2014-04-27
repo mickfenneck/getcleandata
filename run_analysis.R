d7d569284c2c384ca7861a1972db21e5744da60b
Michele Sordo <michele.sordo@gmail.com>
2014-04-27 18:59:52 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,12 @@
-##call as
-# data <- run_analysis(paste(getwd(),"/UCI HAR Dataset/",sep=""))
+## if using source from the directory of this reco, call as
+## data <- run_analysis(paste(getwd(),"/UCI HAR Dataset/",sep=""))
 
 run_analysis <- function(path) {
-    #leggo label e titoli
+    #read activity_labels and titles
     activity_labels <-read.table(paste(path,"/activity_labels.txt",sep=""))
     titles <- read.table(paste(path,"/features.txt",sep=""))
     
-    #leggo le x
+    #read and merge the x datasets
     trainx <- read.table(paste(path,"train/X_train.txt",sep=""))
     testx <- read.table(paste(path,"test/X_test.txt",sep=""))
     x <- rbind(trainx,testx)
@@ -14,23 +14,24 @@ run_analysis <- function(path) {
     eraseCol <- grep("mean()|std()",names(x),invert=TRUE)
     x[eraseCol] <- list(NULL)
     
-    #leggo le y e sostituisco con labels labels
+    #read and merge the y datasets
     trainy <- read.table(paste(path,"train/y_train.txt",sep=""))
     testy <- read.table(paste(path,"test/y_test.txt",sep=""))
     y <- rbind(trainy,testy)
-    #converto la colonna a factor per modificare i levels
     y$V1 <- as.factor(y$V1)
     levels(y$V1) <- activity_labels$V2
     names(y)[1] <- "Activity"
-    #leggo subject
+    #read subjects
     trainSub <- read.table(paste(path,"train/subject_train.txt",sep=""))
     testSub <- read.table(paste(path,"test/subject_test.txt",sep=""))
     k <- rbind(trainSub,testSub)
     names(k)[1] <- "Subject"
-    
-    #unisco y a x
+    #merge y and x to data table
     data <- data.table(cbind(y[1],k,x))
+    #create the tidy dataset
     tidyData <- data[,lapply(.SD, mean), by="Activity,Subject"]
+    #write te dataset to file
     write.table(tidyData, "tidyData.txt")
+    #return the dataset
     tidyData
 }
